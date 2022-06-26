1e6ed5a04084d56b2a98ad1ebc42c4e934ebc25e
GYelejin <me@gy>
2022-06-26 19:12:39 +0300

    new

--- index.js
+++ index.js
@@ -8,3 +8,25 @@ $('#modal-container').click(function(){
   $(this).addClass('out');
   $('body').removeClass('modal-active');
 });
+
+var data = [{
+  title: 'Water Quality',
+  values: [39, 61],
+  labels: ['Portable', 'Non-Portable'],
+  font: {
+
+    size: 34
+
+  },
+  text: 'Water Quality',
+  textposition: 'inside',
+  hole: .6,
+  type: 'pie'
+}];
+
+var layout = {
+  height: 820,
+  width: 600
+};
+
+Plotly.newPlot('Chart1', data, layout);
